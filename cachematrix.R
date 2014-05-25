e3ba355beb42186bb4f9e3a81eb52b04037ecaac
parinshah187 <parin.shah1807@gmail.com>
2014-05-25 13:16:24 -0700

    Update in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+m <- NULL
+      set <- function(y) {
+        x <<- y
+        m <<- NULL
+      }
+      get <- function() x
+      setinv <- function(solve) m <<- solve
+      getinv <- function() m
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 
 }
 
@@ -12,4 +23,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ptm <- proc.time()
+      # from Cache, get Matrix "m" 
+      m <- x$getinv()             
+      # Check if it's null, Retrun value which is read from Cache
+      if(!is.null(m)) {
+        message("retrieving cached data")
+        cat("Process Time : ", proc.time() - ptm)
+        return(m)
+      }
+      # if "m" == NULL, get original Matirx
+      mydata <- x$get()
+      
+      # Calculate inverse using Solve
+      m <- solve(mydata, ...)
+      x$setinv(m)
+      cat("Process Time", proc.time() - ptm)
+      # Returm inverse
+      m
 }
